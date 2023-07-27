ebdb1aad6fc1d53cb6e67f6a4e4515ef9d738208
SwethaGajarla <swethagajarla20@gmail.com>
2023-07-27 06:29:34 +0000

    show and hide clock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,38 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {time: new Date()}
+    console.log('Constructor Called')
+  }
+
+  //   state = {time: new Date()}
+
+  componentDidMount() {
+    this.timerID = setInterval(this.tick, 1000)
+    console.log('componentDidMount() Called')
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+    console.log('componentWillUnmount() Called')
+  }
+
+  tick = () => {
+    this.setState({time: new Date()})
+  }
+
+  render() {
+    const {time} = this.state
+    console.log('Render() Called', time)
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{time.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
